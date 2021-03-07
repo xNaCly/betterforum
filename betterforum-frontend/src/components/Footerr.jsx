0a6ff71a5acf2e5d8eb6422313aade07b866658f
xnacly <matteogropp@gmail.com>
2021-03-07 12:10:36 +0100

    added more links to footer

--- betterforum-frontend/src/components/Footerr.jsx
+++ betterforum-frontend/src/components/Footerr.jsx
@@ -3,7 +3,14 @@ function Footerr() {
 		<div className="footer">
 			<hr id="hr" />
 			<p>&copy; xnacly, 2021</p>
-			<div>
+			<div className="vrhr_center_items">
+				<a id="footer_href" href="https://github.com/xNaCly/betterforum">
+					Repo
+				</a>
+				<a id="footer_href" href="https://github.com/xNaCly/betterforum/blob/master/SETUP.md">
+					Selfhost
+				</a>
+				<div className="vr"></div>
 				<a id="footer_href" href="https://github.com/xNaCly">
 					Github
 				</a>
--- example/src/components/Footerr.jsx
+++ example/src/components/Footerr.jsx
@@ -4,6 +4,13 @@ function Footerr() {
 			<hr id="hr" />
 			<p>&copy; xnacly, 2021</p>
 			<div>
+				<a id="footer_href" href="https://github.com/xNaCly/betterforum">
+					Repo
+				</a>
+				<a id="footer_href" href="https://github.com/xNaCly/betterforum/blob/master/SETUP.md">
+					Selfhost
+				</a>
+				<div className="vr"></div>
 				<a id="footer_href" href="https://github.com/xNaCly">
 					Github
 				</a>
